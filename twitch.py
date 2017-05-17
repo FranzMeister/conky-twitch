785ff080799ec5e418578e6e7136a8bdfe981c43
Erik Dubois <erik.dubois@gmail.com>
2017-05-17 04:34:16 +0200

    first commit - basically a Python 3 compatible version of the original

--- twitch.py
+++ twitch.py
@@ -1,69 +1,72 @@
+
 import json
-import urllib2
+import urllib.request
+import urllib.error
+import urllib.parse
 import os.path
 
 # Settings
-twitch_username = "your-twitch-username-here"
-client_id = "jhf1vq4domnl8a9ix1jl6sbv6w0y7yt&"
+TWITCH_USERNAME = "enter-your-username-here"
+CLIENT_ID = "enter-your-client-id-here"
 
 # Let's fetch and parse data from twitch
-r = urllib2.urlopen("https://api.twitch.tv/kraken/users/"+ twitch_username +"/follows/channels?client_id="+ client_id +"&limit=75").read()
-raw_data = json.loads(r)
-output = ""
+R = urllib.request.urlopen("https://api.twitch.tv/kraken/users/" + TWITCH_USERNAME + "/follows/channels?client_id="+ CLIENT_ID +"&limit=75").read()
+RAW_DATA = json.loads(R)
+OUTPUT = ""
 
-pages = (raw_data["_total"] - 1) / 75
-for page in range(0, pages+1):
-  if (page != 0):
-    r = urllib2.urlopen("https://api.twitch.tv/kraken/users/"+ twitch_username +"/follows/channels?client_id="+ client_id +"&direction=DESC&limit=75&offset=%d&sortby=created_at" % (75 * page)).read()
-    raw_data = json.loads(r)
+PAGES = (RAW_DATA["_total"] - 1) // 75
+for PAGE in range(0, PAGES+1):
+    if (PAGE != 0):
+        R = urllib.request.urlopen("https://api.twitch.tv/kraken/users/"+ TWITCH_USERNAME + "/follows/channels?client_id="+ CLIENT_ID +"&direction=DESC&limit=75&offset=%d&sortby=created_at" % (75 * page)).read()
+        RAW_DATA = json.loads(R)
 
-  # Get followed channels
-  followed_channels = []
-  for channel in raw_data["follows"]:
-    followed_channels.append(channel["channel"]["name"])
+# Get followed channels
+FOLLOWED_CHANNELS = []
+for channel in RAW_DATA["follows"]:
+    FOLLOWED_CHANNELS.append(channel["channel"]["name"])
 
   # Get live streams
-  r = urllib2.urlopen("https://api.twitch.tv/kraken/streams?client_id="+ client_id +"&channel=%s" % ','.join(followed_channels))
-  live_streams = json.loads(r.read())
+R = urllib.request.urlopen("https://api.twitch.tv/kraken/streams?client_id="+ CLIENT_ID + "&channel=%s" % ','.join(FOLLOWED_CHANNELS))
+LIVE_STREAMS = json.loads(R.read())
 
-  for stream in live_streams["streams"]:
+for stream in LIVE_STREAMS["streams"]:
     channel_name = stream["channel"]["display_name"]
 
     # For some strange reason channel status and game sometimes temporarily
     # disappears from Twitch API, causing problems in this script. It this case,
     # we don't show status/game in conky at all.
     try:
-      channel_title = stream["channel"]["status"]
+        channel_title = stream["channel"]["status"]
     except KeyError:
-      channel_title = ""
+        channel_title = ""
 
     try:
-      channel_game = stream["channel"]["game"]
+        channel_game = stream["channel"]["game"]
     except KeyError:
-      channel_game = ""
+        channel_game = ""
 
-    # Build output string
-    output += "${color white}" + channel_name + " is ${color LawnGreen}LIVE"
+    # Build OUTPUT string
+    OUTPUT += "${color black}" + channel_name + " is ${color LawnGreen}LIVE"
 
     if channel_title != "":
-      output += "\n${color white}" + channel_title
+        OUTPUT += "\n${color black}" + channel_title
 
     if channel_game != "":
-      output += "\n${color yellow}" + channel_game
+        OUTPUT += "\n${color yellow}" + channel_game
 
-    output += "\n\n"
+    OUTPUT += "\n\n"
 
 # No live channels?
-if output == "":
-   output = "No live channels"
+if OUTPUT == "":
+    OUTPUT = "No live channels"
 
-# Write output to file
-output = output.replace("#", "\#") # Preventing twitch titles from messing with conky config
-path = os.path.dirname(__file__) + "/"
+# Write OUTPUT to file
+OUTPUT = OUTPUT.replace("#", "\\#") # Preventing twitch titles from messing with conky config
+PATH = os.path.dirname(__file__) + "/"
 
-if path == "/":
-  path = ""
+if PATH == "/":
+    PATH = ""
 
-f = open(path + "streams.txt", "w")
-f.write(output.encode('utf8'))
-f.close()
+F = open(PATH + "streams-test.txt", "wb")
+F.write(OUTPUT.encode('utf8'))
+F.close()
